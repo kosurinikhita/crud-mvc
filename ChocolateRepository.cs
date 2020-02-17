56226ef3d5796aa7db079374b40d63c2408d0142
kosurinikhita <50933537+kosurinikhita@users.noreply.github.com>
2020-02-17 22:22:33 +0530

    Add files via upload

---
+++ ChocolateRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChocolateShop.entity;
+
+namespace ChocolateShop.repository
+{
+    public class ChocolateRepository
+    {
+        public static List<Chocolate> chocolate = new List<Chocolate>();
+        static ChocolateRepository()
+        {
+            chocolate.Add(new Chocolate { Name = "CadburySilk", Price = 100, Protein = 10 });
+            chocolate.Add(new Chocolate { Name = "Milkybar", Price = 25, Protein = 15 });
+            chocolate.Add(new Chocolate { Name = "Kitkat", Price = 10, Protein = 10 });
+        }
+        public IEnumerable<Chocolate> GetChocolateDetails()
+        {
+            return chocolate;
+        }
+        public void AddChocolate(Chocolate chocolates)
+        {
+            chocolate.Add(chocolates);
+        }
+        public Chocolate GetChocolate(string Names)
+        {
+            return chocolate.Find(id => id.Name.Equals(Names));
+        }
+        public void DeleteChocolate(string Names)
+        {
+            Chocolate list = GetChocolate(Names);
+            if(list!=null)
+            {
+                chocolate.Remove(list);
+            }
+        }
+    }
+}
---
+++ DefaultController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChocolateShop.entity;
+using ChocolateShop.repository;
+
+namespace ChocolateShop.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Index
+        ChocolateRepository chocolateRepository;
+        public DefaultController()
+        {
+            chocolateRepository = new ChocolateRepository();
+        }
+        public ActionResult Index()
+        {
+            IEnumerable<Chocolate> chocolate = chocolateRepository.GetChocolateDetails();
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Chocolate chocolate)
+        {
+            chocolateRepository.AddChocolate(chocolate);
+            TempData["Message"] = "Chocolate added successfully!";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string Name)
+        {
+            chocolateRepository.DeleteChocolate(Name);
+            TempData["Message"] = "Chocolate deleted suuccessfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
